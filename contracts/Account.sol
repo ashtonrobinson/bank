554375046715fec60ebca5aa93d7b5d3f76a3183
Ashton Robinson <ashtonr@mit.edu>
2022-06-16 16:34:38 -0400

    created wallet.sol, refactered account.sol, added first basic test

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -1,56 +1,29 @@
 // contracts/Account.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
+
+import "./Wallet.sol";
+
 
 contract Account {
-    //defines the parameters for multisignature wallets
-    uint constant private MAX_OWNERS = 3;   
-    uint constant private NUM_CONFIRMATIONS_REQUIRED = 2;
+    // the associated multisignature wallet
+    Wallet public wallet;
 
-    // each transaction will have the following format
-    struct Transaction {
-        address destination;
-        uint value;
-        bytes data;
-        bool executed;
+    string public firstName;
+    string public lastName;
+
+
+    // deploy the account before setting the associated wallet
+    constructor (string memory _first, string memory _last){
+        firstName = _first;
+        lastName = _last;
     }
 
-    
-    address[] public owners;
-    mapping(address => bool) isOwner;
-
-    
-    Transaction[] public transactions;
-    // mapping that uses the index of each transaction in transactions
-    mapping(uint => mapping (address => bool)) public confirmations;
-    
-    
-
-    //modifiers
-    modifier checkOwners(uint len){
-        require(len == 3);
-        _;
+    function initiateWallet(address[] calldata _owners) external {
+        // call the constructor passing the
+        wallet = new Wallet(_owners);
     }
 
 
-    // create a contract 
-    constructor (address[] memory _owners)
-        checkOwners(_owners.length)
-    {
-        for (uint i = 0; i < _owners.length; i++){
-            address owner = _owners[i];
-
-            // require address to not be the zero address
-            require(owner != address(0), "invalid owner");
-            require(!isOwner[owner], "owner not unique");
-
-
-            isOwner[owner] = true;
-            owners.push(owner);
-        }
-    }
-
-    // function to allow the contract to recieve funds
-    receive() external payable {}
 
 }
---
+++ test/AccountTest.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai');
+
+// testing suite for Account.sol
+describe("Account Contract Testing Suite", function () {
+    // variables used in testing suite created in before and beforeEach 
+    let AccountFactory;
+    let Account; 
+    let first;
+    let last;  
+
+    //redeploy the contract before each test
+    beforeEach(async function () {
+        AccountFactory = await ethers.getContractFactory('Account');
+
+        first = 'Ashton';
+        last = 'Robinson';
+
+        Account = await AccountFactory.deploy(first, last);
+        await Account.deployed();
+    });
+
+    describe("Deployment", function () {
+        //test 1, correct owners
+        it('correct account name', async function () {
+            // only three owners allowed
+            expect(await Account.firstName()).to.equal(first);
+            expect(await Account.lastName()).to.equal(last);
+        });
+    });
+
+
+
+});
