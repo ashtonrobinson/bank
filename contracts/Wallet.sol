0a83dca1c10f44cf7deb50b4fe3778752fcc5229
Ashton Robinson <ashtonr@mit.edu>
2022-06-20 14:42:45 -0400

    added tests for recieve in wallet

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -15,18 +15,28 @@ contract Wallet is Ownable{
         uint value;
         bytes data;
         bool executed;
+        uint numConfirmations;
     }
 
     address[3] public approvers;
     mapping(address => bool) isApprover;
-    
+
+    // each approver is allowed to have one pending txn
+    mapping(address => bool) hasPendingTxn;
+
     // uses the index of each transaction in transactions as key
     mapping(uint => mapping (address => bool)) public confirmations;
     Transaction[] public transactions;
 
-    //event for deposit into this address
-    event Deposit(address indexed from, uint amount, uint balance);
-    
+    event Deposit(address indexed from, uint amount);
+    event SubmitTransaction(
+        address indexed approver,
+        address indexed to, 
+        uint indexed txnNumber,
+        uint value,
+        bytes data
+    );
+
     // only three keys are allowed to be associated with this wallet
     modifier checkApproversLength(uint len){
         require(len == MAX_APPROVERS, "only 3 signers allowed");
@@ -39,10 +49,16 @@ contract Wallet is Ownable{
         _;
     }
 
+    // do not allow the same approver to have more than one pending txn
+    modifier previousTxnNotApproved(){
+        require(!hasPendingTxn[msg.sender], "pending txn exists for this approver");
+        _;
+    }
+
     // check that the contract has enough funds to send
     modifier balanceExists(uint value){
         require(address(this).balance > value, "contract needs more funds");
-        require(value > 0, "cannot send 0 ether");
+        require(value > 0, "cannot send nothing");
         _;
     }
 
@@ -73,7 +89,9 @@ contract Wallet is Ownable{
 
     // function to allow the contract to recieve funds
     receive() external payable {
-        emit Deposit(msg.sender, msg.value, address(this).balance);
+        if (msg.value > 0) {
+            emit Deposit(msg.sender, msg.value);
+        }  
     }
 
     // returns the approvers(addresses) of this specific wallet
@@ -86,18 +104,34 @@ contract Wallet is Ownable{
         return address(this).balance;
     }
 
-    // create a transaction, sending value to dest
+    // create and confirm a transaction from one of the verified addresses
     // requires value > than acct balance and value > 0
     // address cannot be 0x0, the address of the contract itself, or the creator of the wallet
-    function createTransaction(address dest, uint value) 
+    function createTransaction(address _dest, uint _value, bytes memory _data) 
         public
         onlyApprover
-        balanceExists(value)
-        validAddress(dest)
+        previousTxnNotApproved
+        balanceExists(_value)
+        validAddress(_dest)
     {
-        
-    }
+        uint txnNumber = transactions.length;
 
-    
+        //add the transaction data
+        transactions.push(Transaction({
+            destination: _dest,
+            value: _value,
+            data: _data,
+            executed: false,
+            numConfirmations: 1
+        }));
+
+        // mark the creator of the transaction as confirmed to save gas
+        confirmations[txnNumber][msg.sender] = true;
+
+        // mark the sender as having a pending txn to prevent replay attack
+        hasPendingTxn[msg.sender] = true;
+
+        emit SubmitTransaction(msg.sender, _dest, txnNumber, _value, _data);
+    }
 
 }
--- test/WalletTest.js
+++ test/WalletTest.js
@@ -1,18 +1,26 @@
+const { EtherscanProvider } = require('@ethersproject/providers');
 const { expect } = require('chai');
+const { ethers, waffle} = require("hardhat");
 
 // testing suite for Account.sol
 describe("Wallet Contract Testing Suite", function () {
+    let provider;
     // variables used in testing suite created in before and beforeEach 
     let WalletFactory;
     let Wallet; 
     let owner;
+    // list of signer objects
     let signers;
+    // list of 3 signer objects that are the approvers of the wallet
+    let approvers;
+    // addresses of the 3 approvers
     let addresses;
 
     // create the contract factory before anything runs
     before(async function () {
-        WalletFactory = await ethers.getContractFactory('Wallet');
+        provider = waffle.provider;
 
+        WalletFactory = await ethers.getContractFactory('Wallet');
         // pull out owners to test contract with
         [owner, ...signers] = await ethers.getSigners();
     })
@@ -34,11 +42,13 @@ describe("Wallet Contract Testing Suite", function () {
         }
 
         addresses = new Array();
+        approvers = new Array();
 
         const indicies = chooseThree(signers.length);
         for(const index of indicies) {
-            const addr = signers[index].address
-            addresses.push(addr);
+            const appr = signers[index]
+            approvers.push(appr);
+            addresses.push(appr.address);
         }
 
         Wallet = await WalletFactory.deploy(addresses);
@@ -105,13 +115,73 @@ describe("Wallet Contract Testing Suite", function () {
             let addr1 = signerOne.address;
             let addr2 = signerTwo.address;
 
-            await expect(WalletFactory.deploy([addr1, addr2, "0x0000000000000000000000000000000000000000"])).to.be.revertedWith("invalid approver");
+            await expect(WalletFactory.deploy([addr1, addr2, "0x0000000000000000000000000000000000000000"]))
+                .to.be.revertedWith("invalid approver");
         })
-    })
+    });
+
+    // recieve ether and emit an event
+    describe("Receiving Ether", function () {
+        let walletAddr;
+        let balanceBefore;
+
+        beforeEach(async function () {
+            walletAddr = Wallet.address;
+            balanceBefore = await provider.getBalance(walletAddr);
+        });
+
+        //test 1, recieving ether from owner, no ether in contract
+        it('deployer sent 1 ether to contract', async function () {
+            let txn = {to: walletAddr, value: ethers.utils.parseEther('1.0')};
+
+            // confirm the event was recieved
+            await expect(owner.sendTransaction(txn)).to.emit(Wallet, "Deposit")
+                .withArgs(owner.address, ethers.utils.parseEther('1.0'));
+
+            //check that the balance of the account is correct
+            expect(await provider.getBalance(walletAddr)).to.equal(balanceBefore.add(ethers.utils.parseEther('1.0')));
+        });
+
+        //test 2, approver sent zero eth
+        it('approver sent 0 ether to contract', async function () {
+            txn = {to: walletAddr, value: 0};
+
+            const approverOne = approvers[0];
+
+            //no event should be triggered
+            await approverOne.sendTransaction(txn);
+            expect(await provider.getBalance(walletAddr)).to.equal(balanceBefore);
+        });
+
+        //test 3, multiple random addresses sending ether repeatedly
+        it('recieve from random addresses', async function () {
+            value1 = ethers.utils.parseEther('0.009');
+            value2 = ethers.utils.parseEther('0.8');
+            value3 = ethers.utils.parseEther('20.0');
+            txn1 = {to: walletAddr, value: value1};
+            txn2 = {to: walletAddr, value: value2};
+            txn3 = {to: walletAddr, value: value3};
+
+            const randomSingers = signers.filter(sign => !approvers.includes(sign) && sign != owner);
+
+            await expect(randomSingers[0].sendTransaction(txn1)).to.emit(Wallet, "Deposit")
+                .withArgs(randomSingers[0].address, value1);
+            await expect(randomSingers[1].sendTransaction(txn2)).to.emit(Wallet, "Deposit")
+                .withArgs(randomSingers[1].address, value2);
+            await expect(randomSingers[2].sendTransaction(txn3)).to.emit(Wallet, "Deposit")
+                .withArgs(randomSingers[2].address, value3);
+
+            expect(await provider.getBalance(walletAddr)).to.equal(balanceBefore.add(value1).add(value2).add(value3));
+        });
+
+
+    });
 
     // correctly create a transaction
     describe("Transaction Creation", function () {
-        //test 1, 
+        
     });
+
+    
 
 });
